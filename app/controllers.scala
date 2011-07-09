59a8aa08e56626063d9580df59199358ed5075d6
Alvaro Carrasco <alvaro@gravitydev.com>
2011-07-09 13:45:49 -0600

    Some GAE stuff, minor updates, added a calendar

--- app/controllers.scala
+++ app/controllers.scala
@@ -2,10 +2,33 @@ package controllers
 
 import play._
 import play.mvc._
+import play.mvc.results._
+import play.modules.gae._
 
-object Site extends Controller {
+object Site extends ScalaController {
     
     import views.Site._
+    
+    def login (openid:String = "") = {
+    	import com.google.appengine.api.users.UserServiceFactory
+    	import java.util.HashSet
+    	
+    	val us = UserServiceFactory getUserService
+    	val user = us.getCurrentUser
+    	
+    	if (openid != "") {
+    		val loginUrl = us.createLoginURL("/", null, openid, new HashSet[String])
+    		//new Redirect(loginUrl)
+    		
+    		//loginUrl
+    		new Redirect(loginUrl)
+    	} else {
+	    	
+	    	println(user)
+	    	
+	    	html.login()
+    	}
+    }
     
     def index = {
         html.index()
